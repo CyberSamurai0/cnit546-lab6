7f3abe5be3a1ef0da1f1149fc7d2f1a30e08f664
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2025-10-14 23:24:44 -0400

    Add networking for telemetry broadcast

--- Main.py
+++ Main.py
@@ -1,11 +1,14 @@
 # Import Built-In Libraries
 import machine
+import ujson
+import time
 
 # Import Custom Libraries
 import Stepper
 import Buttons
 import LCD
 import DHT
+import Network
 from Thermostat import Thermostat
 
 # Heat/Cool Toggle
@@ -14,6 +17,9 @@ toggle_button = machine.Pin(27, machine.Pin.IN, machine.Pin.PULL_UP)
 # Temperature Up/Down Buttons
 up_button = machine.Pin(12, machine.Pin.IN, machine.Pin.PULL_UP)
 dn_button = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP)
+
+def rpc_callback(topic, msg):
+    print("Received message:", msg)
 
 if __name__ == "__main__":
     print("Running!")
@@ -45,5 +51,20 @@ if __name__ == "__main__":
     # Define GPIO pins for stepper motor
     motor = Stepper.StepperMotor(33, 32, 26, 25)
 
+    # Connect to Wi-Fi
+    wlan = Network.connect_to_wifi("Phone Hotspot", "password")
+    try:
+        mqtt_client = Network.init_MQTT("mqtt.thingsboard.cloud", "token", "", rpc_callback, 1883)
+    except Exception as e:
+        print("MQTT connect failed:", e)
+
     while True:
-        continue
+        telemetry = {
+            "temperature": sensor.temperature,
+            "humidity": sensor.humidity,
+            "setpoint": myThermostat.temperature,
+            "mode": myThermostat.mode
+        }
+        Network.publish_message(mqtt_client, "v1/devices/me/telemetry", ujson.dumps(telemetry))
+        mqtt_client.check_msg()
+        time.sleep(10)
---
+++ Network.py
@@ -0,0 +1,33 @@
+import network
+from umqtt.simple import MQTTClient
+
+def connect_to_wifi(ssid, password):
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('Connecting to Wi-Fi...')
+        wlan.connect(ssid, password)
+        while not wlan.isconnected():
+            pass
+    print('Connected to Wi-Fi:', wlan.ifconfig())
+    return wlan
+
+def init_MQTT(server, user, password, callback, port=1883):
+    client = MQTTClient(
+        client_id="esp32",
+        server=server,
+        user=user,
+        password=password,
+        port=port
+    )
+    client.set_callback(callback)
+    print("Constructed MQTT client")
+    client.connect()
+    print('Connected to MQTT broker')
+    client.subscribe(b"v1/devices/me/rpc/request/+")
+    print("Subscribed to RPC topic")
+    return client
+
+def publish_message(client, topic, message):
+    client.publish(topic, message)
+    print("Published updated cloud telemetry")
